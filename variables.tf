91cafbb973f51262748fc020c84393848afcef8f
Jakub Sokołowski <jakub@status.im>
2019-07-17 23:09:23 -0400

    quote names of variables
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- variables.tf
+++ variables.tf
@@ -1,29 +1,30 @@
 /* REQUIRED -------------------------------------*/
 
-variable cloudflare_token {
+variable "cloudflare_token" {
   description = "Token for interacting with Cloudflare API."
 }
 
-variable digitalocean_token {
+variable "digitalocean_token" {
   description = "Token for interacting with DigitalOcean API."
 }
 
-variable cloudflare_email {
+variable "cloudflare_email" {
   description = "Email address of Cloudflare account."
 }
 
-variable cloudflare_org_id {
+variable "cloudflare_org_id" {
   description = "ID of the CloudFlare organization."
 }
 
 /* GENERAL --------------------------------------*/
 
-variable public_domain {
+variable "public_domain" {
   description = "Domain under which the public sites go."
   default     = "status.im"
 }
 
-variable domain {
+variable "domain" {
   description = "DNS Domain to update"
   default     = "statusim.net"
 }
+
